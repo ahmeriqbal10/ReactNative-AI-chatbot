971912a0d01c3dd9bd67be6d32e3b73ee9f41bed
ahmeriqbal10 <135916430+ahmeriqbal10@users.noreply.github.com>
2025-06-20 02:03:53 +0200

    Adding Deepseek changes

--- Constants.ts
+++ Constants.ts
@@ -1,4 +1,4 @@
-export const OPENAI_API_KEY =  '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';
+export const OPENAI_API_KEY =  '';
 export const OPENAI_API_URL = 'https://api.openai.com/v1/chat/completions'; 
-export const DEEPSEEK_KEY =  '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';
+export const DEEPSEEK_KEY =  '';
 export const DEEPSEEK_URL = 'https://router.huggingface.co/sambanova/v1/chat/completions'; // e.g., 'https://api.openai.com/v1/chat/completions'
